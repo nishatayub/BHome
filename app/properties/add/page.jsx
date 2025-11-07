9ceba707ab8c45096000edaf17b3412a7646a5e5
Nishat Ayub <nishatayub09@icloud.com>
2025-11-07 15:24:11 +0530

    Fix: move dynamic export to top-level before use client directive

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,6 +1,6 @@
-"use client";
+export const dynamic = 'force-dynamic'
 
-export const dynamic = 'force-dynamic';
+"use client";
 
 import React, { useState } from "react";
 import { useSession } from "next-auth/react";
